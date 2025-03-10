bc70d84698716413953985d37fb90bfeb8d3c573
afaninMusfida1 <afaninmusfida1@gmail.com>
2025-03-10 08:54:28 +0700

    delete button create peminjaman admin

--- app/Filament/Resources/PeminjamanResource/Pages/ListPeminjaman.php
+++ app/Filament/Resources/PeminjamanResource/Pages/ListPeminjaman.php
@@ -13,7 +13,7 @@ class ListPeminjaman extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
